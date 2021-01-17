353f6ede2cd2ec7d0d729f80c12d5f7331bdb012
Rivit <albert98g@gmail.com>
2021-01-17 11:22:13 +0100

    start scripts

---
+++ examples/lab/run.sh
@@ -0,0 +1,5 @@
+python3 bathroom.py virtual 127.0.0.1 &
+python3 bedroom.py virtual 127.0.0.2 &
+python3 lobby.py virtual 127.0.0.3 &
+python3 kitchen.py virtual 127.0.0.4 &
+python3 living_room.py virtual 127.0.0.5 &
